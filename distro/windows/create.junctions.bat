06216e1ad9e1c96a66b1613e2309497d3a1c21b3
Matthew Vukomanovic <matthew.vukomanovic@gmail.com>
2016-06-04 19:49:46 +0800

    Include a .gitignore to ignore the junctions which the create junctions create

---
+++ distro/windows/create.junctions.bat
@@ -0,0 +1,3 @@
+::mklink /j ..\..\easyrsa3\bin bin
+::mklink ..\..\easyrsa3\EasyRSA-Start.bat "%CD%\EasyRSA-Start.bat"
+mklink /h  ..\..\easyrsa3\shell-init.sh "%CD%\bin\easyrsa-shell-init.sh"
---
+++ distro/windows/delete.junctions.bat
@@ -0,0 +1,3 @@
+::rmdir ..\..\easyrsa3\bin
+::del ..\..\easyrsa3\EasyRSA-Start.bat
+del ..\..\easyrsa3\shell-init.sh
